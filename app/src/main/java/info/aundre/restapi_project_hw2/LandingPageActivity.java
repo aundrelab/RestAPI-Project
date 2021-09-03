620de04b66a03035babead2dffc1d2ad0c14e3f3
Aundre Labrador <aundrelabrador@gmail.com>
2021-09-03 13:30:45 -0700

    Created login page, took in user input, and set up user class

---
+++ app/src/main/java/info/aundre/restapi_project_hw2/LandingPageActivity.java
@@ -0,0 +1,19 @@
+package info.aundre.restapi_project_hw2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LandingPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+
+    }
+
+}
--- app/src/main/java/info/aundre/restapi_project_hw2/MainActivity.java
+++ app/src/main/java/info/aundre/restapi_project_hw2/MainActivity.java
@@ -1,8 +1,11 @@
 package info.aundre.restapi_project_hw2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create button variable
+        Button buttonForLogin = findViewById(R.id.login);
+
+        // Create on click listener functinality for login button
+        buttonForLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText username1 = findViewById(R.id.username);
+                EditText password1 = findViewById(R.id.password);
+                String username = username1.getText().toString();
+                String password = password1.getText().toString();
+
+                if(validate(username, password)) {
+                    Intent i = new Intent(MainActivity.this, LandingPageActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
     }
+
+    public boolean validate(String username, String password) {
+        return true;
+    }
+
 }
